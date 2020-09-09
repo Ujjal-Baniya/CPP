f61b9fc6ddb3122e4b8991182469d6200fa15522
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-09-09 22:15:43 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
-    int size = 10;
+    int size;
+        cout<<"ENter size of array?";
+    cin>>size;
     int *arr = new int[size] ;
 
-
+    for (int i =0; i<size; i++){
+        cin>>arr[i] ;
+    }
+    for(int i = 0; i<size; i++){
+        cout<<arr[i];
+    }
+    delete arr;
 }
