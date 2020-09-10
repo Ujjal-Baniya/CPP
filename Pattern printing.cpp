903d8010b61b41013046648ef82fee1b7e316902
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-09-10 19:54:33 +0530

    Create Pattern printing.cpp

---
+++ Pattern printing.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<n+1; i++){
+        for(int j=i-1; j>-1; j--){
+            // cout<<(2^j)<<" ";
+            cout<<pow(2,j)<<" ";
+        }
+        cout<<"\n";
+    }
+}
