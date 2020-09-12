3404d91b4e1f76b2c09c3c1f328872c80daeff98
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-09-12 20:28:24 +0530

    Update pair.cpp

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
-using namesapce std;
+using namespace std;
 int main(){
-    set<pair<int,int>> S;
-    s.o
+    set< pair<int, int>> s;
+    int x=10, y = 20;
+    while(x<100){
+        s.insert({x, y});
+        x += 10;
+        y += 10;
+    }
+    int data;
+    cout<<"Enter number: ?";
+    cin>> data;
+    auto it = s.upper_bound({data, INT_MAX});
+    it--;
+    pair <int, int> current = *it;
+   cout<<current.first <<" "<<current.second <<endl;
 }
